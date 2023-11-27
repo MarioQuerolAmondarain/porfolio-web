e8c105ce5916fb513d925b268afc3356c88a5bf7
Mario Querol <marioqueroamondarain@gmail.com>
2023-11-27 20:29:26 +0100

    Dynamic translations with ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'porfolio-web';
+  title = 'Mario Querol';
+
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'es']);
+    translate.setDefaultLang('en');
+
+    translate.use('en');
+  }
+  cambiarLanguage() {
+    this.translate.use('es');
+  }
 }
