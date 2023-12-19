9617100162c0ee9997b09f7871c044bc1470c41d
Mario Querol <marioqueroamondarain@gmail.com>
2023-12-19 20:04:39 +0100

    Creation of landing-page component

---
+++ src/app/templates/landing-page/landing-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss'],
+})
+export class LandingPageComponent implements OnInit {
+  constructor(public translate: TranslateService) {}
+
+  ngOnInit(): void {}
+}
