e2ac524353016e42816ecf53f0d19670909270af
Mario Querol <marioqueroamondarain@gmail.com>
2023-12-24 17:59:50 +0100

    Creation of SkillsComponent

---
+++ src/app/templates/skills/skills.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import * as technologies from './data/tecnologies.json';
+@Component({
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.scss'],
+})
+export class SkillsComponent implements OnInit {
+  technologies: any[] = [];
+  softSkills: any[] = [
+    {
+      name: 'Teamwork',
+    },
+    {
+      name: 'Teamwork',
+    },
+  ];
+
+  constructor(public translate: TranslateService) {
+    this.technologies = Object.values(technologies);
+  }
+
+  ngOnInit(): void {}
+}
--- src/app/templates/templates.module.ts
+++ src/app/templates/templates.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
-  declarations: [LandingPageComponent, AboutMeComponent],
+  declarations: [LandingPageComponent, AboutMeComponent, SkillsComponent],
   imports: [CommonModule, TranslateModule],
-  exports: [LandingPageComponent, AboutMeComponent],
+  exports: [LandingPageComponent, AboutMeComponent, SkillsComponent],
 })
 export class TemplatesModule {}
