35abdd06f305127d806b6bf7cbc324857fbc47b7
Mario Querol <marioqueroamondarain@gmail.com>
2023-12-24 11:51:28 +0100

    Landing page styles changed
    Added animation and responsive layout

--- src/app/templates/landing-page/landing-page.component.ts
+++ src/app/templates/landing-page/landing-page.component.ts
@@ -1,12 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { gsap } from 'gsap';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss'],
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent implements OnInit, AfterViewInit {
   constructor(public translate: TranslateService) {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    gsap.from('.animation', {
+      stagger: 1,
+      opacity: 0,
+      duration: 3,
+      y: -50,
+      ease: 'ease-in',
+    });
+  }
 }
