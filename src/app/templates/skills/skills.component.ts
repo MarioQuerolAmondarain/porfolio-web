1ceb740ecdd5fb1253df199f29c725330bd76b7a
Mario Querol <marioqueroamondarain@gmail.com>
2023-12-25 11:43:44 +0100

    Added new technologies and softskills

--- src/app/templates/skills/skills.component.ts
+++ src/app/templates/skills/skills.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import * as technologies from './data/tecnologies.json';
+import * as softSkills from './data/softskills.json';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
@@ -8,23 +9,11 @@ import * as technologies from './data/tecnologies.json';
 })
 export class SkillsComponent implements OnInit {
   technologies: any[] = [];
-  softSkills: any[] = [
-    {
-      name: 'Teamwork',
-      imgUrl: './../../../assets/tecnologies/teamwork.png',
-    },
-    {
-      name: 'Teamwork',
-      imgUrl: './../../../assets/tecnologies/teamwork.png',
-    },
-    {
-      name: 'Teamwork',
-      imgUrl: './../../../assets/tecnologies/teamwork.png',
-    },
-  ];
+  softSkills: any[] = [];
 
   constructor(public translate: TranslateService) {
     this.technologies = Object.values(technologies);
+    this.softSkills = Object.values(softSkills);
   }
 
   ngOnInit(): void {}
