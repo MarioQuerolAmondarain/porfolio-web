26a637650cc2435ae7c37fe7b9547639646c64a3
Mario Querol <marioquerolamondarain@gmail.com>
2024-01-06 17:03:54 +0100

    My proyects finished

--- src/app/templates/proyects-and-blogs/proyects-and-blogs.component.ts
+++ src/app/templates/proyects-and-blogs/proyects-and-blogs.component.ts
@@ -7,6 +7,28 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./proyects-and-blogs.component.scss'],
 })
 export class ProyectsAndBlogsComponent implements OnInit {
+  // Proyect example
+  // {
+  //   title: 'Mi portfolio',
+  //   subtitle: 'Web personal',
+  //   description: 'Proyecto personal para mostrar mi portfolio',
+  //   image: './../../../assets/proyects/office.jpg',
+  //   link: 'https://www.helloworld-school.com/blog/del-aula-a-la-oficina-consejos-y-experiencia-de-un-junior',
+  //   technologies: [
+  //     {
+  //       name: 'Angular',
+  //       image: './../../../assets/tecnologies/angular.svg',
+  //     },
+  //     {
+  //       name: 'NodeJS',
+  //       image: './../../../assets/tecnologies/api.png',
+  //     },
+  //     {
+  //       name: 'MongoDB',
+  //       image: './../../../assets/tecnologies/git.png',
+  //     },
+  //   ],
+  // },
   proyects: any[] = [];
   blogs: any[] = [
     {
