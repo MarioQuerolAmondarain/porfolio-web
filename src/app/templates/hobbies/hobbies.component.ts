cd8a5c3466f8a22f28cf0da3c336c29b850ffa91
Mario Querol <marioquerolamondarain@gmail.com>
2024-01-06 19:33:02 +0100

    Changed hobbies layout and text

--- src/app/templates/hobbies/hobbies.component.ts
+++ src/app/templates/hobbies/hobbies.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-hobbies',
   templateUrl: './hobbies.component.html',
-  styleUrls: ['./hobbies.component.scss']
+  styleUrls: ['./hobbies.component.scss'],
 })
 export class HobbiesComponent implements OnInit {
+  constructor(public translate: TranslateService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
